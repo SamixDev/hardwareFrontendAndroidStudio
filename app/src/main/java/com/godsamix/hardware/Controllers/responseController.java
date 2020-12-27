26fd4f700da1d8a9fade9b1d4334a95a2c43da21
samixdev <samer.hd.ah@gmail.com>
2020-12-27 11:54:11 +0200

    add constructors for response controller for status and msg

--- app/src/main/java/com/godsamix/hardware/Controllers/responseController.java
+++ app/src/main/java/com/godsamix/hardware/Controllers/responseController.java
@@ -1,4 +1,31 @@
 package com.godsamix.hardware.Controllers;
 
+import com.google.gson.annotations.SerializedName;
+
 public class responseController {
+    @SerializedName("status")
+    private  String status;
+    @SerializedName("message")
+    private  String message;
+
+    public responseController(String status, String message) {
+        this.status = status;
+        this.status = status;
+    }
+
+    public responseController() {
+
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
 }
+
